2abb0c226face91d06c9b4d82b2d69e0fa4dad2d
Pyry Kontio <pyry.kontio@drasa.eu>
2016-09-16 02:51:39 +0300

    Setting session cookie works now.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -5,6 +5,7 @@ extern crate dotenv;
 #[macro_use] extern crate log;
 extern crate env_logger;
 extern crate hyper;
+#[macro_use]  extern crate lazy_static;
 
 use dotenv::dotenv;
 use std::env;
@@ -14,6 +15,11 @@ use std::collections::BTreeMap;
 use hyper::header::{SetCookie, CookiePair};
 use pencil::{Pencil, Request, PencilResult, redirect, abort};
 
+lazy_static! {
+    static ref SITE_DOMAIN : String = { dotenv().ok(); env::var("GANBARE_SITE_DOMAIN")
+    .unwrap_or_else(|_| "ENV VAR GANBARE_SITE_DOMAIN NOT SET".into()) };
+}
+
 fn hello(request: &mut Request) -> PencilResult {
     let mut context = BTreeMap::new();
     context.insert("title".to_string(), "akusento.ganba.re".to_string());
@@ -22,22 +28,25 @@ fn hello(request: &mut Request) -> PencilResult {
 
 fn login(request: &mut Request) -> PencilResult {
     let conn = ganbare::db_connect().map_err(|_| abort(500).unwrap_err())?;
-    let email;
-    let plaintext_pw;
+    let user;
     {
         let login_form = request.form();
-        email = login_form.get("email").map(String::as_ref).unwrap_or("");
-        plaintext_pw = login_form.get("password").map(String::as_ref).unwrap_or("");
+        let email = login_form.get("email").map(String::as_ref).unwrap_or("");
+        let plaintext_pw = login_form.get("password").map(String::as_ref).unwrap_or("");
+        user = ganbare::auth_user(&conn, email, plaintext_pw)
+            .map_err(|e| match e.kind() {
+                    &ErrorKind::AuthError => abort(401).unwrap_err(),
+                    _ => abort(500).unwrap_err(),
+                })?;
     };
-    let user = ganbare::auth_user(&conn, email, plaintext_pw)
-        .map_err(|e| match e.kind() {
-                &ErrorKind::AuthError => abort(401).unwrap_err(),
-                _ => abort(500).unwrap_err(),
-            })?;
-    let session = ganbare::start_session(&conn, &user, request.request.remote_addr.ip()).map_err(|_| abort(500).unwrap_err())?;
+    
+    let session = ganbare::start_session(&conn, &user, request.request.remote_addr.ip())
+        .map_err(|_| abort(500).unwrap_err())?;
+
     let mut cookie = CookiePair::new("session_id".to_owned(), ganbare::session_id(&session));
-    cookie.path = Some("/path".to_owned());
-    cookie.domain = Some("example.com".to_owned());
+    cookie.path = Some("/".to_owned());
+    cookie.domain = Some(SITE_DOMAIN.to_owned());
+
     redirect("/", 303).map(|mut r| { r.set_cookie(SetCookie(vec![cookie])); r })
 }
 
@@ -50,13 +59,16 @@ fn main() {
     app.set_debug(true);
     app.set_log_level();
     env_logger::init().unwrap();
-    debug!("* Running on http://localhost:5000/");
+    debug!("* Running on http://localhost:5000/, serving at {:?}", *SITE_DOMAIN);
 
     app.get("/", "hello", hello);
     app.post("/login", "login", login);
 
     let binding = match env::var("GANBARE_SERVER_BINDING") {
-        Err(_) => { println!("Specify the ip address and port to listen (e.g. 0.0.0.0:80) in envvar GANBARE_SERVER_BINDING!"); return; },
+        Err(_) => {
+            println!("Specify the ip address and port to listen (e.g. 0.0.0.0:80) in envvar GANBARE_SERVER_BINDING!");
+            return;
+        },
         Ok(ok) => ok,
     };
     println!("Ready to run at {}", binding);
