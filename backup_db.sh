e74af8882a4945aeff145f5b625f0e623af34c31
Pyry Kontio <pyry.kontio@drasa.eu>
2016-12-30 03:48:55 +0200

    Improved the backup script to read the audio and image dirs from .env

--- backup_db.sh
+++ backup_db.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 command -v pg_dump >/dev/null 2>&1 || { echo >&2 "I require pg_dump but it's not installed. Aborting."; exit 1; }
 command -v xxd >/dev/null 2>&1 || { echo >&2 "I require xxd but it's not installed. Aborting."; exit 1; }
+GANBARE_AUDIO_DIR="audio"
+GANBARE_IMAGES_DIR="images"
+source .env 2> /dev/null
 if [ -z $1 ]
 then
 echo "Usage: backup_db.sh <name of the database>"
 else
 BACKUP_DIR="backups/$(date -u +"%Y-%m-%dT%H-%M-%SZ")-$(xxd -l 4 -p /dev/urandom )"
-mkdir -p $BACKUP_DIR
+mkdir -p $BACKUP_DIR || exit 1
+echo "Backing up to: $BACKUP_DIR Audio dir: $GANBARE_AUDIO_DIR Images dir: $GANBARE_IMAGES_DIR"
 pg_dump -F d -h localhost $1 -f "$BACKUP_DIR/db_dump"
-cp -r "audio" "${BACKUP_DIR}/"
-cp -r "images" "${BACKUP_DIR}/"
+cp -r "$GANBARE_AUDIO_DIR" "${BACKUP_DIR}/"
+cp -r "$GANBARE_IMAGES_DIR" "${BACKUP_DIR}/"
 fi
