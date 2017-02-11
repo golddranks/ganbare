2408142e0b57b4e090301c8d3f714cedb9048339
Pyry Kontio <pyry.kontio@drasa.eu>
2017-02-11 14:14:49 +0200

    Improved content deployment scripts

--- deploy_new_items_production.sh
+++ deploy_new_items_production.sh
@@ -9,11 +9,15 @@ DEPLOY_AUDIO_DIR=/srv/ganbare_production/audio
 DEPLOY_IMAGES_DIR=/srv/ganbare_production/images
 
 pg_dump -h localhost --data-only --column-inserts -t audio_bundles -t narrators -t skill_nuggets -t audio_files -t words -t quiz_questions -t question_answers $LOCAL_DB_NAME -f new_items_temp.sql
+echo "Moving the SQL data to the server"
 rsync -r new_items_temp.sql $DEPLOY_SERVER:
 rm new_items_temp.sql
+echo "SSH to server and insert the data"
 ssh $DEPLOY_SERVER /bin/sh <<EOF
 psql -h localhost -d $DEPLOY_DB_NAME -f new_items_temp.sql
 rm new_items_temp.sql
 EOF
+echo "Upload audio files to the server"
 rsync -r --exclude $LOCAL_AUDIO_DIR/trash $LOCAL_AUDIO_DIR/ $DEPLOY_SERVER:$DEPLOY_AUDIO_DIR
+echo "Upload image files to the server"
 rsync -r --exclude $LOCAL_IMAGES_DIR/trash $LOCAL_IMAGES_DIR/ $DEPLOY_SERVER:$DEPLOY_IMAGES_DIR
