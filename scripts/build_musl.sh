ba73c7bd49838c1dd942532beee113f546f0bd13
Pyry Kontio <pyry.kontio@drasa.eu>
2017-02-21 18:01:36 +0200

    Dockerfile refactoring. WIP.

--- scripts/build_musl.sh
+++ scripts/build_musl.sh
@@ -5,7 +5,8 @@ echo "$(( $(cat build_number.txt 2> /dev/null || echo "0") + 1 ))" > build_numbe
 rm target/x86_64-unknown-linux-musl/release/ganbare 2> /dev/null || true
 docker rm -v ganbare_builder 2> /dev/null || true
 scripts/build_static.sh && \
-docker run -it --name ganbare_builder -v $PWD:/workdir -e "BUILD_FEATURES=$BUILD_FEATURES" golddranks/ganbare_build && \
+BUILD_BUILDTIME_PEPPER=$(cat .env.ganbare_buildtime_pepper)
+docker run -it --name ganbare_builder -v $PWD:/workdir -e "BUILD_FEATURES=$BUILD_FEATURES" -e "GANBARE_BUILDTIME_PEPPER=$BUILD_BUILDTIME_PEPPER" golddranks/ganbare_build && \
 docker cp ganbare_builder:/etc/ssl/certs certs_temp && \
 docker rm -v ganbare_builder && \
 docker build --no-cache -f scripts/Dockerfile.run -t golddranks/ganbare_run . && \
