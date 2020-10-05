7ebf4b70ffafd96821197a64324792e0170ceb18
Pyry Kontio <pyry.kontio@drasa.eu>
2020-10-06 04:58:50 +0900

    Streamlined scripts.

--- scripts/backup_store.sh
+++ scripts/backup_store.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
+
+. scripts/local.env
+
 command -v pg_dump >/dev/null 2>&1 || { echo >&2 "I require pg_dump but it's not installed. Aborting."; exit 1; }
 command -v xxd >/dev/null 2>&1 || { echo >&2 "I require xxd but it's not installed. Aborting."; exit 1; }
 
-DB_NAME=${1:?Usage: backup_db.sh <name of the database> <name of the backup>}
-BACKUP_NAME=${2:?Usage: backup_db.sh <name of the database> <name of the backup>}
+BACKUP_NAME=${1:?Usage: backup_db.sh <name of the backup>}
 
-GANBARE_AUDIO_DIR="audio"
-GANBARE_IMAGES_DIR="images"
+LOCAL_AUDIO_DIR="audio"
+LOCAL_IMAGES_DIR="images"
 
 if [ -f ./.env ]; then
 	. .env
 fi
 
-if [ ! -d "$GANBARE_AUDIO_DIR" ]; then
-	echo "The audio dir $GANBARE_AUDIO_DIR doesn't exist!" && exit 1
+if [ ! -d "$LOCAL_AUDIO_DIR" ]; then
+	echo "The audio dir $LOCAL_AUDIO_DIR doesn't exist!" && exit 1
 fi
 
-if [ ! -d "$GANBARE_IMAGES_DIR" ]; then
-	echo "The image dir $GANBARE_IMAGES_DIR doesn't exist!" && exit 1
+if [ ! -d "$LOCAL_IMAGES_DIR" ]; then
+	echo "The image dir $LOCAL_IMAGES_DIR doesn't exist!" && exit 1
 fi
 
 echo "Starting backup!"
 BACKUP_DIR="backups/$(date -u +"%Y-%m-%dT%H-%M-%SZ")-$(xxd -l 4 -p /dev/urandom )_$BACKUP_NAME"
 mkdir -p "$BACKUP_DIR" || { echo "Can't create the directory $BACKUP_DIR! Aborting."; exit 1; }
-echo "Backing up to: $BACKUP_DIR Audio dir: $GANBARE_AUDIO_DIR Images dir: $GANBARE_IMAGES_DIR"
-pg_dump -F d -h localhost "$DB_NAME" -f "$BACKUP_DIR/db_dump"
-cp -r "$GANBARE_AUDIO_DIR" "${BACKUP_DIR}/"
-cp -r "$GANBARE_IMAGES_DIR" "${BACKUP_DIR}/"
+echo "Backing up to: $BACKUP_DIR Audio dir: $LOCAL_AUDIO_DIR Images dir: $LOCAL_IMAGES_DIR"
+PGPASSWORD="$LOCAL_DB_PASSWORD" pg_dump -F d -h "$LOCAL_DB_HOSTNAME" -U "$LOCAL_DB_USER" "$DB_NAME" -f "$BACKUP_DIR/db_dump"
+cp -r "$LOCAL_AUDIO_DIR" "${BACKUP_DIR}/"
+cp -r "$LOCAL_IMAGES_DIR" "${BACKUP_DIR}/"
--- scripts/deploy_new_items.sh
+++ scripts/deploy_new_items.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-LOCAL_DB_NAME=ganbare_dev
-LOCAL_AUDIO_DIR=audio
-LOCAL_IMAGES_DIR=images
+. local.env
 
 ENVFILE={1:?Usage: deploy_new_items.sh  <prd.env|stg.env>}
 . "$ENVFILE"
 
 echo "Local DB: $LOCAL_DB_NAME Remote DB: $DEPLOY_DB_NAME"
 
-pg_dump -h localhost --data-only --column-inserts -t audio_bundles -t narrators -t skill_nuggets -t audio_files -t words -t exercises -t exercise_variants -t quiz_questions -t question_answers "$LOCAL_DB_NAME" -f new_items_temp.sql
+PGPASSWORD="$LOCAL_DB_PASSWORD" pg_dump -h "$LOCAL_DB_HOSTNAME" -U "$LOCAL_DB_USER" --data-only --column-inserts -t audio_bundles -t narrators -t skill_nuggets -t audio_files -t words -t exercises -t exercise_variants -t quiz_questions -t question_answers "$LOCAL_DB_NAME" -f new_items_temp.sql
 echo "Moving the SQL data to the server"
 rsync -r new_items_temp.sql "$DEPLOY_SERVER:"
 rm new_items_temp.sql
