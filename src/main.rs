445762813d6e58972f6fca1a574eef58cc2019b5
Pyry Kontio <pyry.kontio@drasa.eu>
2020-10-06 16:36:49 +0900

    Host name checking was a bit too restrictive so relaxed it a bit.

--- src/main.rs
+++ src/main.rs
@@ -184,15 +184,15 @@ fn csrf_check(req: &mut Request) -> Option<PencilResult> {
     let method_mutating = !req.method().safe();
     let url = req.url.path();
 
-    if req.host.hostname != &**SITE_DOMAIN {
-        return Some(Ok(bad_request(format!("The host field is wrong. Expected: {}, Got: {}",
-                                           &**SITE_DOMAIN,
-                                           req.host.hostname))));
-    }
-
     if method_mutating || (*PARANOID && url.starts_with("/api")) {
         // Enable anti-CSRF heuristics:
         // when the method is POST, DELETE etc., or if the request uses the HTTP API.
+
+        if req.host.hostname != &**SITE_DOMAIN {
+            return Some(Ok(bad_request(format!("The host field is wrong. Expected: {}, Got: {}",
+                                            &**SITE_DOMAIN,
+                                            req.host.hostname))));
+        }
 
         if let Some(&Origin { host: Host { ref hostname, .. }, .. }) = origin {
             if hostname != &**SITE_DOMAIN {
