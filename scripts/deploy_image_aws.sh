9d33b72b1082be868a34b26db44b27abd5038a65
Pyry Kontio <pyry.kontio@drasa.eu>
2020-10-07 05:43:20 +0900

    AWS ECS deploying works.

--- scripts/deploy_image_aws.sh
+++ scripts/deploy_image_aws.sh
@@ -4,3 +4,4 @@ scripts/build_release.sh
 
 docker tag accent_ganbare:latest_prd 437938062036.dkr.ecr.eu-north-1.amazonaws.com/accent-ganbare-web:latest_prd
 docker push 437938062036.dkr.ecr.eu-north-1.amazonaws.com/accent-ganbare-web:latest_prd
+aws ecs update-service --cluster ganbare-cluster --service accent-ganbare-service --force-new-deployment
