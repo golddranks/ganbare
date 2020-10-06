0f9588457caf14a933f78081e3ef61cbbf57fbaa
Pyry Kontio <pyry.kontio@drasa.eu>
2020-10-06 17:38:10 +0900

    Deploy to GCP works.

--- scripts/deploy_image_gcp.sh
+++ scripts/deploy_image_gcp.sh
@@ -4,3 +4,4 @@ scripts/build_release.sh
 
 docker tag accent_ganbare:latest_prd gcr.io/striped-rhino-291704/accent_ganbare:latest_prd
 docker push gcr.io/striped-rhino-291704/accent_ganbare:latest_prd
+gcloud run deploy accent-ganbare-web --image gcr.io/striped-rhino-291704/accent_ganbare:latest_prd --platform managed --region europe-north1
